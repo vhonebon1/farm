516f26302133f0fbb8120fb2ea6347c67b083747
verity <verity.honebon@royalacademy.org.uk>
2019-10-04 07:23:29 +0200

    Basic dropdown functionality

---
+++ src/components/dropdown.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Chevron from '../icons/chevron.svg';
+
+class Dropdown extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      options: this.props.options,
+      selected: 0,
+      showOptions: false
+    }
+  }
+
+  handleSelect = (index) => {
+    this.props.customOnClick(index);
+    this.setState({ selected: index, showOptions: false });
+  }
+
+  toggleShowOptions = () => {
+    this.setState({ showOptions: !this.state.showOptions });
+  }
+
+  render() {
+    const { showOptions, options, selected } = this.state;
+    return(
+      <div className="dropdown__wrapper">
+        <div className="dropdown">
+          {options[selected].label}
+          <img src={Chevron} onClick={() => this.toggleShowOptions()} />
+        </div>
+        { showOptions &&
+          <div className="dropdown__options">
+            { options.map((option, index) =>
+              <div className={`dropdown__option ${index == this.state.selected ? 'selected' : 'unselected'}`} onClick={() => this.handleSelect(index)}>{option.label}</div>
+            )}
+          </div>
+        }
+      </div>
+    )
+  }
+
+}
+
+export default Dropdown;
--- src/components/productTile.jsx
+++ src/components/productTile.jsx
@@ -1,20 +1,58 @@
 import React from 'react';
+import Dropdown from './dropdown';
 
-const ProductTile = ({ product }) =>
-  <div className="productTile">
-    <div className="productTile__image--wrapper">
-      <img className="productTile__image" src={product.media[0].url} />
-    </div>
-    <div className="productTile__info">
-      <h2 className="productTile__info--name">{product.name}</h2>
-      <div className="productTile__info--producer">{product.producer.name}</div>
-      <div className="productTile__info--measurement">{product.measurement.displayName}</div>
-      <div className="productTile__info--priceWrapper">
-        <div className="productTile__info--price">£{product.price.pence / 100}</div>
-        <div className="productTile__info--price">{product.pricePerUnit}</div>
-      </div>
-      <button className="button">Add</button>
-    </div>
-  </div>
+class ProductTile extends React.Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       selectedVariant: null,
+     }
+   }
+
+   handleVarientSelect = (id) => {
+     this.setState({ selectedVariant: id })
+   }
+
+   variantOptions = () => {
+     const { variants } = this.props.product;
+     const options = [];
+     for (var variant in variants) {
+       options.push({'label': variants[variant].measurement.displayName, 'id': variant})
+     }
+     return options;
+   }
+
+   render() {
+     const { name, producer, measurement, price, pricePerUnit, media, variants } = this.props.product;
+     const { selectedVariant } = this.state;
+     const displayPrice = selectedVariant ? variants[selectedVariant].price.pence : price.pence;
+     const displayPricePerUnit = selectedVariant ? variants[selectedVariant].pricePerUnit : pricePerUnit;
+
+     return(
+       <div className="productTile">
+         <div className="productTile__image--wrapper">
+           <img className="productTile__image" src={media[0].url} />
+         </div>
+         <div className="productTile__info">
+           <h2 className="productTile__info--name">{name}</h2>
+           <div className="productTile__info--producer">{producer.name}</div>
+           { variants.length > 1 ?
+             <Dropdown
+               options={this.variantOptions()}
+               selected={this.selectedVariant}
+               customOnClick={this.handleVarientSelect}
+              />
+             : <div className="productTile__info--measurement">{measurement.displayName}</div>
+           }
+         <div className="productTile__info--priceWrapper">
+           <div className="productTile__info--price">£{displayPrice / 100}</div>
+           <div className="productTile__info--price">{displayPricePerUnit}</div>
+         </div>
+         <button className="button">Add</button>
+       </div>
+     </div>
+     )
+   }
+}
 
 export default ProductTile;
