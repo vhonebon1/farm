3f2cdea334ef449acae9aae550669367e491f69a
verity <verity.honebon@royalacademy.org.uk>
2019-10-07 07:42:03 +0200

    Remove yarn lock and set up

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Header from './components/header';
+import ProductGrid from './components/productGrid';
+import { shallow, mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import Store from './components/store';
 
 it('renders without crashing', () => {
+  const store = Store();
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders header on load', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.contains(<Header />)).toBe(true)
+});
+
+it('gets product data', () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.state().products.length).not.toBe(0);
+})
